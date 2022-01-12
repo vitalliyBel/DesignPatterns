f142f9a9a6bdc502c57709dca5581ae4509f29d4
Vitaliy Belinskiy <vitalybelinsky@gmail.com>
2022-01-12 22:23:44 +0300

    created Decorator pattern package. Creating an example based on the Witcher

---
+++ src/DecoratorPattern/TheWitcher.java
@@ -0,0 +1,10 @@
+package DecoratorPattern;
+
+public interface TheWitcher {
+
+    String whatDoesTheWitcherLookLike();
+    String signsOfTheWitcher();
+
+    int basicStrength();
+    int basicDexterity();
+}
---
+++ src/DecoratorPattern/TheWitcherDecor.java
@@ -0,0 +1,36 @@
+package DecoratorPattern;
+
+public abstract class TheWitcherDecor implements TheWitcher {
+
+    protected final TheWitcher witcher;
+
+    public TheWitcherDecor(TheWitcher witcher) {
+        this.witcher = witcher;
+    }
+
+    @Override
+    public String whatDoesTheWitcherLookLike() {
+        return witcher.whatDoesTheWitcherLookLike();
+    }
+
+    @Override
+    public String signsOfTheWitcher(){
+
+        return witcher.signsOfTheWitcher();
+
+    }
+
+    @Override
+    public int basicStrength(){
+        return witcher.basicStrength();
+
+    }
+
+    @Override
+    public int basicDexterity(){
+        return witcher.basicDexterity();
+    }
+
+}
+
+
