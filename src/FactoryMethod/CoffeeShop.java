732adf8d5a19d5d0432659d4e8a71104cd43a9d0
AleksandrChernov1988 <Aleksander88Cherhov@yandex.by>
2022-01-12 20:32:28 +0200

    add classes Coffee and CoffeeTpe

--- src/FactoryMethod/CoffeeShop.java
+++ src/FactoryMethod/CoffeeShop.java
@@ -1,4 +1,12 @@
 package FactoryMethod;
 
-public class CoffeeShop {
+public abstract class CoffeeShop {
+    public Coffee orderCoffee(CoffeeType coffeeType){
+        Coffee coffee = createCoffee(coffeeType);
+        coffee.makeCoffee();
+        coffee.pourIntoCoup();
+        System.out.println("Get your coffee");
+        return coffee;
+    }
+    protected abstract Coffee createCoffee(CoffeeType coffeeType);
 }
