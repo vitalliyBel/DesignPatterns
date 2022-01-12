80eb094731f3b5ad057d7bb7a55825919bd218b9
Vitaliy Belinskiy <vitalybelinsky@gmail.com>
2022-01-12 16:46:07 +0300

    Initial commit

---
+++ src/CreationalPatterns/PatternsCreational.java
@@ -0,0 +1,6 @@
+package CreationalPatterns;
+
+public class PatternsCreational { public static void main(String[] args) {
+    // write your code here
+}
+}
---
+++ src/StructuralPatterns/PatternsStructural.java
@@ -0,0 +1,6 @@
+package StructuralPatterns;
+
+public class PatternsStructural {public static void main(String[] args) {
+    // write your code here
+}
+}
