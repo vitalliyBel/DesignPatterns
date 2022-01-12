fa995e0cf8df9de2942bd5df204480e93aaba942
Vitaliy Belinskiy <vitalybelinsky@gmail.com>
2022-01-12 17:32:42 +0300

    created new branch

--- src/CreationalPatterns/PatternsCreational.java
+++ src/CreationalPatterns/PatternsCreational.java
@@ -1,6 +1,6 @@
 package CreationalPatterns;
 
 public class PatternsCreational { public static void main(String[] args) {
-    // write your code here
+    System.out.println("start to work");
 }
 }
